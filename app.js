8e64cb63de2357b1dc2658639590bc64d656d703
Sifat07 <sifat140693@gmail.com>
2022-11-02 19:48:39 +0600

    feat(db): initialized and connected mongodb and added blogs view

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connect to mongodb & listen for requests
+const dbURI =
+  "mongodb+srv://sifatjasim:S140693khan@cluster0.ywgsqpe.mongodb.net/nodejs-blog?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware & static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 app.get("/", (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
-  // res.send('<p>about page</p>');
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+// blog routes
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { blogs: result, title: "All blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
